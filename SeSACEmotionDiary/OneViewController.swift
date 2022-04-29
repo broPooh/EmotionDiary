a0d936224792ddd1bf5fcfb5f628b1efd3544466
broMac <yhs4705@naver.com>
2022-04-29 16:20:22 +0900

    학습후 커밋

---
+++ SeSACEmotionDiary/OneViewController.swift
@@ -0,0 +1,72 @@
+//
+//  OneViewController.swift
+//  SeSACEmotionDiary
+//
+//  Created by bro on 2022/04/29.
+//
+
+import UIKit
+
+class OneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(self, #function)
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        print(self, #function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(self, #function)
+    }
+    
+    
+
+}
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(self, #function)
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        print(self, #function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(self, #function)
+    }
+    
+    
+
+}
+
+
+class ThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(self, #function)
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        print(self, #function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(self, #function)
+    }
+    
+    
+
+}
